9a9644b3e379ece68c9a12ab1faaf07a92e9daa3
mohamadaliraqi <107443181+mohamadaliraqi@users.noreply.github.com>
2022-06-18 06:15:39 +0300

    Update java1.js

--- java1.js
+++ java1.js
@@ -1,59 +1,18 @@
-    <script type="text/javascript">
 
-     (function(){
+$(document).ready(function(){ 
+ var touch  = $('#resp-menu');
+ var menu  = $('.menu');
+ 
+ $(touch).on('click', function(e) {
+  e.preventDefault();
+  menu.slideToggle();
+ });
+ 
+ $(window).resize(function(){
+  var w = $(window).width();
+  if(w > 600 && menu.is(':hidden')) {
+   menu.removeAttr('style');
+  }
+ });
+});
 
-      var animenuToggle = document.querySelector('.animenu__toggle'),
-
-          animenuNav    = document.querySelector('.animenu__nav'),
-
-          hasClass = function( elem, className ) {
-
-            return new RegExp( ' ' + className + ' ' ).test( ' ' + elem.className + ' ' );
-
-          },
-
-          toggleClass = function( elem, className ) {
-
-            var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ' ) + ' ';
-
-            if( hasClass(elem, className ) ) {
-
-              while( newClass.indexOf( ' ' + className + ' ' ) >= 0 ) {
-
-                newClass = newClass.replace( ' ' + className + ' ' , ' ' );
-
-              }
-
-              elem.className = newClass.replace( /^\s+|\s+$/g, '' );
-
-            } else {
-
-              elem.className += ' ' + className;
-
-            }
-
-          },      
-
-          animenuToggleNav =  function (){    
-
-            toggleClass(animenuToggle, "animenu__toggle--active");
-
-            toggleClass(animenuNav, "animenu__nav--open");    
-
-          }
-
-      if (!animenuToggle.addEventListener) {
-
-          animenuToggle.attachEvent("onclick", animenuToggleNav);
-
-      }
-
-      else {
-
-          animenuToggle.addEventListener('click', animenuToggleNav);
-
-      }
-
-    })()
-
-    </script>
